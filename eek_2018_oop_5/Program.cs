603e1858e8db8bdcbe187eac2e0d06fb61607d5f
Ott Tooming <OttTooming@gmail.com>
2018-02-16 14:31:58 +0200

    Initial commit

---
+++ eek_2018_oop_5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eek_2018_oop_5
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
